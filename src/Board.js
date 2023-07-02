fb7881896a63f63971438aa4b042c959f0e3120f
Paul Fioravanti <paul.fioravanti@gmail.com>
2023-07-02 21:33:47 +1000

    Create rows and styles

--- src/Board.js
+++ src/Board.js
@@ -1,9 +1,23 @@
 import { Square } from "./Square";
 
 export function Board() {
+  const rows = [...chunks([...Array(9).keys()], 3)];
+
   return (
     <div className="game-board">
-      <Square value="X" />
+      {rows.map(row =>
+        <div className="board-row">
+          {row.map(squareNumber =>
+            <Square value={squareNumber} />
+          )}
+        </div>
+      )}
     </div>
   );
+}
+
+function* chunks(array, chunkSize) {
+  for (let i = 0; i < array.length; i += chunkSize) {
+    yield array.slice(i, i + chunkSize);
+  }
 }
